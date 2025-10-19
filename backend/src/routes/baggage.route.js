036de91f2127ac0f3b043089d2da52d27501e3f1
JimFrankS <37932960+JimFrankS@users.noreply.github.com>
2025-10-19 18:58:54 +0200

    Further route update for better baggage search

--- backend/src/routes/baggage.route.js
+++ backend/src/routes/baggage.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { lostBaggage, claimBaggage } from '../controllers/baggage.controller.js';
+import { lostBaggage, searchBaggage, claimBaggage } from '../controllers/baggage.controller.js';
 import { sanitizeInput } from '../middleware/sanitizeInput.middleware.js';
 import { provinceValidator } from '../middleware/destinationValidator.middleware.js';
 
